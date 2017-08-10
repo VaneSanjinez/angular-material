76a485d76cb8a8ea10cd157fea606f127e160009
Vanessa Sanjinez <vane.sanji@gmail.com>
2017-08-10 10:16:34 -0400

    Added hamer to module and tsconfig, also added material links to index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule // <--Add this
   ],
   providers: [],
   bootstrap: [AppComponent]
