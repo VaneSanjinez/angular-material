04a74c1ece1e22927e3931b04e80c0c27602fdaf
Vanessa Sanjinez <vane.sanji@gmail.com>
2017-08-10 21:37:56 -0400

    Angular-cli, added theme at styles part, karma modified styles, created data.json for handle the material, commented styles.css not making references to prebuilt themes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MaterialModule } from '@angular/material';
+import {MaterialModule} from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
